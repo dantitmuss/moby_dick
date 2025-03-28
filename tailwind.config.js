97c39118c252ca507516b942d6f7dde45c8bad87
Dan Titmuss <dantitmusscomedy@gmail.com>
2025-03-28 17:08:28 -0500

    added basic tialwind to the site

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Adjust the path to your templates if needed.
+    content: [
+        "./templates/*.html",
+        "./templates/**/*.html"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
