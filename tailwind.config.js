f765e0c09af4fdbd6192f921ed6e734e9baf188a
Dan Titmuss <dantitmusscomedy@gmail.com>
2025-03-28 20:49:38 -0500

    Added base HTML and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+// tailwind.config.js
 module.exports = {
-    // Adjust the path to your templates if needed.
-    content: [
-        "./templates/*.html",
-        "./templates/**/*.html"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    "./*.html",           // We'll test with a root-level test.html
+    "./templates/*.html", // Your actual Flask templates
+    "./templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
