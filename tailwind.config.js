b11929b9ffa8bee6f9ea1292a8b4cbe016a2c279
Dan Titmuss <dantitmusscomedy@gmail.com>
2025-04-04 19:07:56 -0500

    styling updates (fade in and more)

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         // Make it the default serif font
         serif: ['"IM Fell English"', 'serif'],
       },
+      animation: {
+        fade: "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
     },
   },
   plugins: [],
